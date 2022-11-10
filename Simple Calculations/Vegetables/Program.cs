68710732f51ba7ffa1642257201fe5c903c6f463
DaniDagami <117672277+DaniDagami@users.noreply.github.com>
2022-11-10 22:32:05 +0200

    Add files via upload

---
+++ Simple Calculations/Vegetables/Program.cs
@@ -0,0 +1,5 @@
+﻿var PriceVeg = double.Parse(Console.ReadLine());
+var PriceFruit = double.Parse(Console.ReadLine());
+var kgVeg = double.Parse(Console.ReadLine());
+var kgFruit= double.Parse(Console.ReadLine());
+Console.WriteLine(Math.Round((PriceVeg*kgVeg+PriceFruit*kgFruit)/1.94, 2));
