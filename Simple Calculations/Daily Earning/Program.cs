e0a924af2b7e63b73847d3407c8b1a914c95be90
DaniDagami <117672277+DaniDagami@users.noreply.github.com>
2022-11-10 22:26:45 +0200

    Add files via upload

---
+++ Simple Calculations/Daily Earning/Program.cs
@@ -0,0 +1,8 @@
+﻿var workdays = int.Parse(Console.ReadLine());
+var money = double.Parse(Console.ReadLine());
+var dollarkurs= double.Parse(Console.ReadLine());
+var monthly = workdays * money;
+var yearly = monthly * 12 + monthly * 2.5;
+var danuk = yearly * 0.25;
+var cleanyearly = (yearly-danuk)*dollarkurs;
+Console.WriteLine(Math.Round(cleanyearly / 365, 2));
