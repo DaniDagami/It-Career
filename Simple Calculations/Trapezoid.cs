58a299fcf6b17b74c18b5f5a5048cdce11823117
DaniDagami <117672277+DaniDagami@users.noreply.github.com>
2022-11-10 22:18:20 +0200

    Delete Trapezoid.cs

--- Simple Calculations/Trapezoid.cs
+++
@@ -1,5 +0,0 @@
-var b1 = double.Parse(Console.ReadLine());
-var b2 = double.Parse(Console.ReadLine());
-var h = double.Parse(Console.ReadLine());
-var area = (b1 + b2) * h / 2;
-Console.WriteLine("Trapezoid area= " + area);
