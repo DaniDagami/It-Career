401a140cfa9f9a436f5df0b48b9901c343eef019
DaniDagami <117672277+DaniDagami@users.noreply.github.com>
2022-11-10 22:28:57 +0200

    Add files via upload

---
+++ Simple Calculations/Tiles/Program.cs
@@ -0,0 +1,8 @@
+﻿var N = double.Parse(Console.ReadLine());
+var W = double.Parse(Console.ReadLine());
+var L = double.Parse(Console.ReadLine());
+var M = double.Parse(Console.ReadLine());
+var O = double.Parse(Console.ReadLine());
+var Area = (N * N - M * O) / (W * L);
+Console.WriteLine(Math.Round(Area, 2));
+Console.WriteLine(Math.Round(Area * 0.2, 2));
