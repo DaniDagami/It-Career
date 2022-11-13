55150db0276b6525b90fff4d0b7bd3adf85745c6
DaniDagami <117672277+DaniDagami@users.noreply.github.com>
2022-11-13 02:51:28 +0200

    Add files via upload

---
+++ Simple-Conditions/SumSec/Program.cs
@@ -0,0 +1,6 @@
+﻿Console.WriteLine("Enter seconds:");
+int a = int.Parse(Console.ReadLine());
+int b = int.Parse(Console.ReadLine());
+int c = int.Parse(Console.ReadLine());
+int total = a + b + c;
+Console.WriteLine($"{total/60:d}:{total%60:d2}");     // stava i Console.WriteLine("{0:d}:{1:d2}", total/60, total%60)
