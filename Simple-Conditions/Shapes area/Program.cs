19e8ff7f1b5d0567af829fb9d218f4fc92de9c60
DaniDagami <117672277+DaniDagami@users.noreply.github.com>
2022-11-13 03:20:51 +0200

    Add files via upload

---
+++ Simple-Conditions/Shapes area/Program.cs
@@ -0,0 +1,23 @@
+﻿string shape = Console.ReadLine();
+if (shape == "square")
+{
+    var a = double.Parse(Console.ReadLine());
+    Console.WriteLine(Math.Round(a*a,3));
+}
+else if (shape == "rectangle")
+{
+    var a = double.Parse(Console.ReadLine());
+    var b = double.Parse(Console.ReadLine());
+    Console.WriteLine(Math.Round(a*b,3));
+}
+else if (shape == "circle")
+{
+    var a = double.Parse(Console.ReadLine());
+    Console.WriteLine(Math.Round(Math.PI * a * a,3));
+}
+else if (shape == "triangle")
+{
+    var a = double.Parse(Console.ReadLine()); 
+    var b = double.Parse(Console.ReadLine());
+    Console.WriteLine(Math.Round(a * b/2,3));
+}
