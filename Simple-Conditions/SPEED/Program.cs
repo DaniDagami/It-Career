58e186fddf8c2a40f26cffa9693e92e053011dc6
DaniDagami <117672277+DaniDagami@users.noreply.github.com>
2022-11-13 10:27:54 +0200

    Update Program.cs

--- Simple-Conditions/SPEED/Program.cs
+++ Simple-Conditions/SPEED/Program.cs
@@ -1,6 +1,6 @@
-﻿var a = double.Parse(Console.ReadLine());
-if (a > 150 && a < 1000) Console.WriteLine("ultra fast");
-else if (a > 50 && a < 150) Console.WriteLine("fast");
-else if (a > 10 && a < 50) Console.WriteLine("avarage");
-else if (a < 10) Console.WriteLine("slow");
+var a = double.Parse(Console.ReadLine());
+if (a<=10) Console.WriteLine("slow");
+else if (a<=50) Console.WriteLine("average");
+else if (a<=150) Console.WriteLine("fast");
+else if (a<=1000) Console.WriteLine("ultra fast");
 else Console.WriteLine("extremly fast");
