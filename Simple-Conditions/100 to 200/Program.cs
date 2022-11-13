3872f5d0c4d28e868062d6c96ab8846296b384f2
DaniDagami <117672277+DaniDagami@users.noreply.github.com>
2022-11-13 02:49:20 +0200

    Add files via upload

---
+++ Simple-Conditions/100 to 200/Program.cs
@@ -0,0 +1,4 @@
+﻿int a= int.Parse(Console.ReadLine());
+if (a < 100) Console.WriteLine("Less than 100");
+else if (a > 100 && a < 200) Console.WriteLine("Between 100 and 200");
+else Console.WriteLine("Greater than 200");
