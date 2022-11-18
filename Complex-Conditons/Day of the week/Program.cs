c89d91de383f4c1cf5a0a1a01005e3b93cfbe36f
DaniDagami <117672277+DaniDagami@users.noreply.github.com>
2022-11-18 18:52:31 +0200

    Add files via upload

---
+++ Complex-Conditons/Day of the week/Program.cs
@@ -0,0 +1,12 @@
+﻿int a = int.Parse(Console.ReadLine());
+switch (a)
+{
+    case 1: Console.WriteLine("Monday"); break;
+    case 2: Console.WriteLine("Tuesday"); break;
+    case 3: Console.WriteLine("Wendnesday"); break;
+    case 4: Console.WriteLine("Thursday"); break;
+    case 5: Console.WriteLine("Friday"); break;
+    case 6: Console.WriteLine("Saturday"); break;
+    case 7: Console.WriteLine("Sunday"); break;
+    default: Console.WriteLine("Error"); break;
+}
