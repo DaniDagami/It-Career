974ca47f751ae96d43ec5196f665f81656bdc924
DaniDagami <117672277+DaniDagami@users.noreply.github.com>
2022-11-18 18:53:01 +0200

    Add files via upload

---
+++ Complex-Conditons/Fruit and vegetable/Program.cs
@@ -0,0 +1,4 @@
+﻿string a = Console.ReadLine();
+if (a == "banana"|| a=="apple"|| a=="kiwi" || a=="cherry"|| a=="lemon"||a=="grapes") Console.WriteLine("fruit");
+else if (a == "tomato"||a=="cucumber"||a=="pepper"|| a=="carrot") Console.WriteLine("vegetable");
+else Console.WriteLine("unknown");
