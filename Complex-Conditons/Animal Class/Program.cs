6ad4fcd93540bde3fdfc6ed0ee72d9e70d95cb1f
DaniDagami <117672277+DaniDagami@users.noreply.github.com>
2022-11-18 18:51:14 +0200

    Add files via upload

---
+++ Complex-Conditons/Animal Class/Program.cs
@@ -0,0 +1,4 @@
+﻿string animal = Console.ReadLine();
+if (animal == "dog") Console.WriteLine("mammal");
+else if (animal == "snake" || animal=="tortoise"|| animal=="crocodile") Console.WriteLine("reptile");
+else Console.WriteLine("unknown");
