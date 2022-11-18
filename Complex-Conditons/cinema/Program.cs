b4ab46be3e19226f4ea2e8c9a146e690ddf76abc
DaniDagami <117672277+DaniDagami@users.noreply.github.com>
2022-11-18 18:51:52 +0200

    Add files via upload

---
+++ Complex-Conditons/cinema/Program.cs
@@ -0,0 +1,19 @@
+﻿string type = Console.ReadLine();
+int row = int.Parse(Console.ReadLine());
+int column = int.Parse(Console.ReadLine());
+
+if (type=="Premiere")
+{
+    double result = row*column*12;
+    Console.WriteLine($"{result:f2} leva");
+}
+else if (type=="Normal")
+{
+    double result = row*column*7.5;
+    Console.WriteLine($"{result:f2} leva");
+}
+else if (type=="Discount")
+{
+    double result = row*column*5;
+    Console.WriteLine($"{result:f2} leva");
+}
