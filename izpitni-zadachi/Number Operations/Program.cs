3f7dc8d04edfa86b2b7db14d3b9286ec23e30104
DaniDagami <117672277+DaniDagami@users.noreply.github.com>
2022-11-20 17:51:49 +0200

    Add files via upload

---
+++ izpitni-zadachi/Number Operations/Program.cs
@@ -0,0 +1,34 @@
+﻿int n1 = int.Parse(Console.ReadLine());
+int n2 = int.Parse(Console.ReadLine());
+char op = char.Parse(Console.ReadLine());
+if (op == '+')
+{
+    if ((n1 + n2) % 2 == 0) Console.WriteLine($"{n1} {op} {n2} = {n1 + n2} - even");
+    else Console.WriteLine($"{n1} {op} {n2} = {n1 + n2} - odd");
+}
+else if (op == '-')
+{
+    if ((n1 - n2) % 2 == 0) Console.WriteLine($"{n1} {op} {n2} = {n1 - n2} - even");
+    else Console.WriteLine($"{n1} {op} {n2} = {n1 - n2} - odd");
+}
+else if (op == '*')
+{
+    if (n1 * n2 % 2 == 0) Console.WriteLine($"{n1} {op} {n2} = {n1 * n2} - even");
+    else Console.WriteLine($"{n1} {op} {n2} = {n1 * n2} - odd");
+}
+else if (op == '/')
+{   if (n2 != 0)
+    {
+        double del = n1 / n2;
+        Console.WriteLine($"{n1} {op} {n2} = {del:f2}");
+    }
+    else Console.WriteLine($"Cannot divide {n1} by zero");
+}
+else if (op == '%')
+{
+    if (n2 != 0)
+    {
+        Console.WriteLine($"{n1} {op} {n2} = {n1%n2}");
+    }
+    else Console.WriteLine($"Cannot divide {n1} by zero");
+}
