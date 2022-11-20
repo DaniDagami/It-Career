a69db5b61e270601e291fd7d09cc5e341af42517
DaniDagami <117672277+DaniDagami@users.noreply.github.com>
2022-11-20 17:51:08 +0200

    Add files via upload

---
+++ izpitni-zadachi/journey/Program.cs
@@ -0,0 +1,19 @@
+﻿double budget = double.Parse(Console.ReadLine());
+string season = Console.ReadLine();
+if (budget <= 100)
+{
+    Console.WriteLine("Somewhere in Bulgaria");
+    if (season == "summer") Console.WriteLine($"Camp - {Math.Round(budget * 0.3,2):f2}");
+    else if (season == "winter") Console.WriteLine($"Hotel - {Math.Round(budget * 0.7,2):f2}");
+}
+else if (budget <= 1000)
+{
+    Console.WriteLine("Somewhere in Balkans");
+    if (season == "summer") Console.WriteLine($"Camp - {Math.Round(budget * 0.4,2):f2}");
+    else if (season == "winter") Console.WriteLine($"Hotel - {Math.Round(budget * 0.8,2):f2}");
+}
+else if (budget > 1000)
+{
+    Console.WriteLine("Somewhere in Europe");
+    Console.WriteLine($"Hotel - {Math.Round(budget * 0.9,2):f2}");
+}
