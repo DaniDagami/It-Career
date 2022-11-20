cf602b896bad0dc00376d6641c119d0d7f7abd01
DaniDagami <117672277+DaniDagami@users.noreply.github.com>
2022-11-20 17:54:18 +0200

    Add files via upload

---
+++ izpitni-zadachi/Tommas/Program.cs
@@ -0,0 +1,20 @@
+﻿int pochd = int.Parse(Console.ReadLine());
+if (pochd >= 1 && pochd <= 365)
+{
+    int rabd = 365 - pochd;
+    int playtime = rabd * 63 + pochd * 127;
+    if (playtime < 30000)
+    {
+        int razlika = 30000 - playtime;
+        Console.WriteLine("Tom sleeps well");
+        Console.WriteLine($"{razlika / 60} hours and {razlika % 60} minutes less for play");
+    }
+    else
+    {
+        int razlika = playtime - 30000;
+        Console.WriteLine("Tom will run away");
+        Console.WriteLine($"{razlika / 60} hours and {razlika % 60} minutes more for play");
+
+    }
+}
+else Console.WriteLine("Error");
