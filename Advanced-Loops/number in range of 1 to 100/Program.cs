1a7f678b23cb6fb0d5037cbfe38eb2b2da2ebf93
DaniDagami <117672277+DaniDagami@users.noreply.github.com>
2022-11-24 22:23:49 +0200

    Add files via upload

---
+++ Advanced-Loops/number in range of 1 to 100/Program.cs
@@ -0,0 +1,7 @@
+﻿int n = int.Parse(Console.ReadLine());
+while (n < 1 || n > 100)
+{
+    Console.WriteLine("Invalid number");
+    n = int.Parse(Console.ReadLine());
+}
+Console.WriteLine($"The number is:{n}");
