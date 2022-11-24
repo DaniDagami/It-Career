9f806b54ffff27281c6ca872f40abe254519420b
DaniDagami <117672277+DaniDagami@users.noreply.github.com>
2022-11-24 22:18:53 +0200

    Add files via upload

---
+++ Advanced-Loops/Even numberss/Program.cs
@@ -0,0 +1,19 @@
+﻿while (true)
+{
+    try
+    {
+        Console.Write("Enter even number: ");
+        int n = int.Parse(Console.ReadLine());
+        if (n % 2 == 0)
+        {
+            Console.WriteLine($"Even number entered: {n}");
+            break;
+        }
+        else Console.WriteLine("Number is not even"); 
+
+    }
+    catch
+    {
+        Console.WriteLine("Invalid number");
+    }
+}
