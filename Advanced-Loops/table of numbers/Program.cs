f42eec2f518246b0a1f2e15fa9bf0203f5968e02
DaniDagami <117672277+DaniDagami@users.noreply.github.com>
2022-11-24 22:27:09 +0200

    Add files via upload

---
+++ Advanced-Loops/table of numbers/Program.cs
@@ -0,0 +1,14 @@
+﻿int n = int.Parse(Console.ReadLine());
+for (int row = 0; row < n; row++)
+{
+    for (int col = 0; col < n; col++)
+    {
+        int num = row + col+1;
+        if (num > n)
+        {
+            num = 2 * n - num;
+        }
+        Console.Write(num + " ");
+    }
+    Console.WriteLine();
+}
