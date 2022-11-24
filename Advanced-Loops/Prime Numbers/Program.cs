b90ea680633118637633c32e45a55735499827c4
DaniDagami <117672277+DaniDagami@users.noreply.github.com>
2022-11-24 22:25:06 +0200

    Add files via upload

---
+++ Advanced-Loops/Prime Numbers/Program.cs
@@ -0,0 +1,9 @@
+﻿int n = int.Parse(Console.ReadLine());
+int br = 1;
+for (int i = 2; i <= n; i++)
+{
+    if (n % i == 0) br++;
+}
+if (n < 1) Console.WriteLine("Not Prime");
+else if (br == 2) Console.WriteLine("Prime");
+else Console.WriteLine("Not Prime");
