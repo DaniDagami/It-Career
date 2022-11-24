5b151958aa3cfa5e9d0a2eb52a6581c46c03b24a
DaniDagami <117672277+DaniDagami@users.noreply.github.com>
2022-11-24 22:25:46 +0200

    Add files via upload

---
+++ Advanced-Loops/Pyramid of numbers/Program.cs
@@ -0,0 +1,23 @@
+﻿int n = int.Parse(Console.ReadLine());
+int num = 1;
+for (int i = 1; i <= n; i++)
+{
+    for (int j = 1; j <= i; j++)
+    {   
+        if (j > 1)
+        {
+            Console.Write(" ");
+        }
+        Console.Write(num);
+        num++;
+        if (num > n)
+        {
+            break;
+        }
+    }
+    Console.WriteLine();
+    if (num > n)
+    {
+        break;
+    }
+}
