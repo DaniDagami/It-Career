733d0da808d48e6ce785ddeb3013ebec55ae9ed5
DaniDagami <117672277+DaniDagami@users.noreply.github.com>
2022-11-25 23:46:40 +0200

    Add files via upload

---
+++ Drawing-with-Loops/House/Program.cs
@@ -0,0 +1,18 @@
+﻿int n = int.Parse(Console.ReadLine());
+var stars = 1;
+if (n % 2 == 0) stars++;
+for (int i = 0; i < (n+1)/2; i++)
+{
+    var pad = (n - stars) / 2;
+    for(int j = 1; j <= pad; j++) Console.Write('-');
+    for(int j = 1; j <= stars; j++) Console.Write('*');
+    for (int j = 1; j <= pad; j++) Console.Write('-');
+    stars += 2;
+    Console.WriteLine();
+}
+for (int i = 1; i <=n/2; i++)
+{
+    Console.Write('|');
+    for (int j = 1; j <= n - 2; j++) Console.Write('*');
+    Console.WriteLine('|'); 
+}
