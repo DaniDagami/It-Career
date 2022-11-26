04a745eaf50f673c5bd335343680838b81ca5ec7
DaniDagami <117672277+DaniDagami@users.noreply.github.com>
2022-11-26 02:44:31 +0200

    Add files via upload

---
+++ Declaring-And-Calling-Methods/Printing a Triangle/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+class Program
+{
+    static void PrintLine(int n)
+    {
+        for (int i = 1; i <=n ; i++)
+        {
+            for (int j = 1; j <= i; j++)
+            {
+                Console.Write(j + " ");
+            }
+            Console.WriteLine();
+        }
+        for (int i = n - 1; i >= 1; i--)
+        {
+            for (int j = 1; j <= i; j++)
+            {
+                Console.Write(j + " ");
+            }
+            Console.WriteLine();
+        }
+    }
+    static void Main()
+    {
+        int n = int.Parse(Console.ReadLine());
+        PrintLine(n);
+    }
+}
