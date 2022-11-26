d4d6271a17d8beb07f1b4c960f31b918364983bb
DaniDagami <117672277+DaniDagami@users.noreply.github.com>
2022-11-26 02:11:49 +0200

    Add files via upload

---
+++ Izpitni-Zadachi-Na-Loops/Drawing a fortress/Program.cs
@@ -0,0 +1,31 @@
+﻿int n = int.Parse(Console.ReadLine());
+if(n<3||n>1000)return;
+Console.Write('/');
+for(int i = 1; i <= n/2; i++)Console.Write('^');
+Console.Write('\\');
+for (int i = 1; i <= 2*n-4-n/2*2; i++)Console.Write('_');
+Console.Write('/');
+for (int i = 1; i <= n / 2; i++) Console.Write('^');
+Console.Write('\\');
+Console.WriteLine();
+for (int i = 1; i <= n-3; i++)
+{
+    Console.Write('|');
+    for (int j = 1; j <= 2 * n - 2; j++) Console.Write(' ');
+    Console.Write('|');
+    Console.WriteLine();
+}
+Console.Write('|');
+for (int i = 1; i <= n / 2 + 1; i++) Console.Write(' ');
+for (int i = 1; i <= 2 * n - 4 - n / 2 * 2; i++) Console.Write('_');
+for (int i = 1; i <= n / 2 + 1; i++) Console.Write(' ');
+Console.Write('|');
+Console.WriteLine();
+Console.Write('\\');
+for (int i = 1; i <= n / 2; i++) Console.Write('_');
+Console.Write('/');
+for (int i = 1; i <= 2 * n - 4 - n / 2 * 2; i++) Console.Write(' ');
+Console.Write('\\');
+for (int i = 1; i <= n / 2; i++) Console.Write('_');
+Console.Write('/');
+Console.WriteLine();
