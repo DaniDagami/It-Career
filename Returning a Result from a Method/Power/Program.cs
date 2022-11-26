c54332a5cbcfe7995028de868b8d30469786985e
DaniDagami <117672277+DaniDagami@users.noreply.github.com>
2022-11-26 02:48:14 +0200

    Add files via upload

---
+++ Returning a Result from a Method/Power/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+class Program
+{
+    static double Power(double n, double p)
+    {
+        double n1=1;
+        for (int i = 1; i <= p; i++)
+        {
+            n1 *= n;
+        }
+        return n1;
+    }
+    static void Main()
+    {
+        double n = double.Parse(Console.ReadLine());
+        double p = double.Parse(Console.ReadLine());
+        Console.WriteLine(Power(n, p));
+    }
+}
