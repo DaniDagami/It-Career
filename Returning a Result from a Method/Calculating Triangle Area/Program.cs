7f0102337b1958e3dded673175fbe53a384dbac2
DaniDagami <117672277+DaniDagami@users.noreply.github.com>
2022-11-26 02:47:37 +0200

    Add files via upload

---
+++ Returning a Result from a Method/Calculating Triangle Area/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+class Program
+{
+    static double Input(double a,double ha)
+    {
+        return (a * ha / 2);
+    }
+    static void Main()
+    {
+        double a = double.Parse(Console.ReadLine());
+        double ha = double.Parse(Console.ReadLine());
+        double area = Input(a, ha);
+        Console.WriteLine(area);
+    }
+}
