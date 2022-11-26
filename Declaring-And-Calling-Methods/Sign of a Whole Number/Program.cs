faa57cec58760d1fe6a4e0b7010308c192619048
DaniDagami <117672277+DaniDagami@users.noreply.github.com>
2022-11-26 02:45:02 +0200

    Add files via upload

---
+++ Declaring-And-Calling-Methods/Sign of a Whole Number/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+class Sign_of_a_Whole_Number
+{
+    static void PrintSign(int n)
+    {
+        if (n > 0) Console.WriteLine($"The number {n} is positive.");
+        else if (n < 0) Console.WriteLine($"The number {n} is negative.");
+        else if (n == 0) Console.WriteLine($"The number {n} is zero.");
+    }
+    static void Main()
+    {
+        int n = int.Parse(Console.ReadLine());
+        PrintSign(n);
+        
+    }
+}
