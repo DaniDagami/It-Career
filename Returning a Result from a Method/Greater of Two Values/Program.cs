08b8b296a47c0f275dd8a4cace464cf51f1bfa50
DaniDagami <117672277+DaniDagami@users.noreply.github.com>
2022-11-26 02:46:58 +0200

    Add files via upload

---
+++ Returning a Result from a Method/Greater of Two Values/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+class Program
+{
+    static int GetMax(int f, int s)
+    {
+        if (f > s) return f;
+        else return s;
+    }
+    static char GetMax(char f, char s)
+    {
+        if (f > s) return f;
+        return s;
+    }
+    static string GetMax(string f, string s)
+    {
+        if (f.CompareTo(s) >= 0) return f;
+        else return s;
+    }
+    static void Main()
+    {
+        var type = Console.ReadLine();
+        if (type=="int")
+        {
+            int f = int.Parse(Console.ReadLine());
+            int s = int.Parse(Console.ReadLine());
+            Console.WriteLine(GetMax(f, s));
+        }
+        else if (type == "char")
+        {
+            char f = char.Parse(Console.ReadLine());
+            char s = char.Parse(Console.ReadLine());
+            Console.WriteLine(GetMax(f, s));
+        }
+        else if (type == "string")
+        {
+            string f = Console.ReadLine();
+            string s = Console.ReadLine();
+            Console.WriteLine(GetMax(f, s));
+        }
+    }
+}
