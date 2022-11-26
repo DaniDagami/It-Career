41cb7ad6f9926f1c86fa71ffb38820beaf986221
DaniDagami <117672277+DaniDagami@users.noreply.github.com>
2022-11-26 02:43:56 +0200

    Add files via upload

---
+++ Declaring-And-Calling-Methods/Draw a Filled Square/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+class Program
+{
+    static void PrintHeaderFooter(int n)
+    {
+        for (int i = 1; i <= 2 * n; i++) Console.Write('-');
+        Console.WriteLine();
+    }
+    static void PrintBody(int n)
+    {
+        for (int i = 1; i <= n-2 ; i++)
+        {
+            Console.Write('-');
+            for (int j = 1; j <= n - 1; j++) Console.Write("\\/");
+            Console.Write('-');
+            Console.WriteLine();
+        }
+    }
+    static void Main()
+    {
+        int n = int.Parse(Console.ReadLine());
+        PrintHeaderFooter(n);
+        PrintBody(n);
+        PrintHeaderFooter(n);
+    }
+}
