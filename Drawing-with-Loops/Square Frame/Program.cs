fbb87abd4f340e293639660381377cc9c9fcb621
DaniDagami <117672277+DaniDagami@users.noreply.github.com>
2022-11-26 02:04:09 +0200

    Add files via upload

---
+++ Drawing-with-Loops/Square Frame/Program.cs
@@ -0,0 +1,13 @@
+﻿int n = int.Parse(Console.ReadLine());
+Console.Write("+");
+for(int i = 1; i <= n-2; i++) Console.Write(" -");
+Console.WriteLine(" +");
+for (int j = 1; j <= n - 2; j++)
+{
+    Console.Write("|");
+    for (int i = 1; i <= n - 2; i++) Console.Write(" -");
+    Console.WriteLine(" |");
+}
+Console.Write("+");
+for (int i = 1; i <= n - 2; i++) Console.Write(" -");
+Console.WriteLine(" +");
