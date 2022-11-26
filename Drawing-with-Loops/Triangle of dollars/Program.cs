a5d99793707cce3532d3f8fbf3bd248462f330eb
DaniDagami <117672277+DaniDagami@users.noreply.github.com>
2022-11-26 02:06:07 +0200

    Add files via upload

---
+++ Drawing-with-Loops/Triangle of dollars/Program.cs
@@ -0,0 +1,10 @@
+﻿int n = int.Parse(Console.ReadLine());
+for (int i = 1; i <= n; i++)
+{
+    Console.Write("$");
+    for (int j = 1; j < i; j++)
+    {
+        Console.Write(" $");
+    }
+    Console.WriteLine();
+}
