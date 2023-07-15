acbab2544137f5f4c78269aaac3bde3bb93e74ad
DaniDagami <daniel.g.boychev@gmail.com>
2023-07-15 17:22:33 +0300

    MODULE2 START

---
+++ First Year/Module02/10.1/zad1/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace zad1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+            Console.WriteLine($"Min = {arr.Min()}");
+            Console.WriteLine($"Max = {arr.Max()}");
+            Console.WriteLine($"Sum = {arr.Sum()}");
+            Console.WriteLine($"Average = {arr.Average()}");
+        }
+    }
+}
